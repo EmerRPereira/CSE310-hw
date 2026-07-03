30bfab3bf8ff2bed9dc09f7dbc5dc91cf4ba6d40
EMERSON RONALD PEREIRA <104523253+EmerRPereira@users.noreply.github.com>
2026-07-03 15:53:52 -0300

    Add HelloWorld.kt with a welcome message

---
+++ HelloWorld.kt
@@ -0,0 +1,8 @@
+// HelloWorld.kt
+// This is a simple Kotlin program that prints a welcome message
+
+fun main() {
+    println("Hello World!")
+    println("Welcome to CSE 310 - Applied Programming")
+    println("Emerson Ronald Pereira")
+}
